c4e2d30f7b6b0868c9b288dad3c76a160a89aa44
Luam-Novais <luampabl014@gmail.com>
2026-03-20 07:55:23 -0300

    adiciona Zustand como dependência e cria página de criação de conta

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,7 @@
-
-export default function Layout({children}: {children: React.ReactNode}){
-    return (
-      <main className="h-full w-full py-8 px-4 flex justify-center items-center">
-        <section>{children}</section>
-      </main>
-    );
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="h-full w-full py-8 px-4 flex justify-center items-center">
+      <section>{children}</section>
+    </main>
+  );
 }
