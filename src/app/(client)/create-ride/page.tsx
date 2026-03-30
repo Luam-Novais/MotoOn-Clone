3a55aa4c8d1e2ba3caf70f959a431ff7b425a4c8
Luam-Novais <luampabl014@gmail.com>
2026-03-30 07:42:16 -0300

    fix: consertando bug do input de calendario vazando a tela

--- src/app/(client)/create-ride/page.tsx
+++ src/app/(client)/create-ride/page.tsx
@@ -52,10 +52,10 @@ export default function Page() {
   }
 
   return (
-    <div className="py-6 px-4 flex flex-col gap-4 bg-gray-300">
+    <div className="py-6 px-4 flex flex-col gap-4 bg-gray-300 max-w-full">
       <Title className="text-[#090909] font-bold">Para onde Vamos ?</Title>
       <p className="text-[#222]">Nos diga para onde deseja ir, que levaremos você.</p>
-      <form className="bg-container py-8 px-4 rounded-md shadow-black/50 shadow-md flex flex-col gap-y-10 overflow-x-hidden" onSubmit={handleSubmit(onSubimit)} action="">
+      <form className="bg-container py-8 px-4 rounded-md shadow-black/50 shadow-md flex flex-col gap-y-10 max-w-full" onSubmit={handleSubmit(onSubimit)} action="">
         {countFormPage === 1 && (
           <div className="flex flex-col gap-y-10 overflow-hidden">
             <Input label="Nome" type="text" register={register('name')} />
