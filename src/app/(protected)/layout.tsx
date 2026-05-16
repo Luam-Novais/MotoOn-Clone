18ece8f50d35cb8b400dd3c57bda881eb5a1f6c5
Luam-Novais <luampabl014@gmail.com>
2026-05-15 21:14:41 -0300

    teste 2

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -14,7 +14,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const { permission } = useNotificationStore();
   return (
     <AuthProvider className="mb-40 relative">
-      {permission !== 'granted' && <EnableNotificationsPrompt showModal={showModalNotification} handleClose={() => setShowModalNotification(false)} handleOpen={() => setShowModalNotification(true)} />}
+      {/* {permission !== 'granted' && <EnableNotificationsPrompt showModal={showModalNotification} handleClose={() => setShowModalNotification(false)} handleOpen={() => setShowModalNotification(true)} />} */}
       <ToastContainer position="top-right" autoClose={3000} theme="dark" />
       {children}
       <Header href={pathname} />
